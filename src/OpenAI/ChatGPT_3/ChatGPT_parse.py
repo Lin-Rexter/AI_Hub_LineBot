38ead60e0a53dcfc73337193998952df99dbaba4
Lin-Rexter <aaaa35963596@gmail.com>
2023-04-01 12:48:45 +0800

    update code

--- src/OpenAI/ChatGPT_3/ChatGPT_parse.py
+++ src/OpenAI/ChatGPT_3/ChatGPT_parse.py
@@ -1,12 +1,29 @@
+import os
+from pathlib import Path
+# 讀取.env檔
+from dotenv import load_dotenv
 #第三方作者包裝的官方API
 from revChatGPT.V3 import Chatbot
 
-def ChatGPT_Reply(Api_Key, Model, Ask) -> str:
-    chatbot = Chatbot(
-        api_key = Api_Key,
-        engine = Model
-    )
+# 讀取.env檔環境變量
+env_path = Path(__file__).resolve().parents[3] / '.env'
+load_dotenv(env_path)
 
+# 獲取OpenAI Api的Key
+Openai_Api_Key = os.getenv('OPENAI_API_KEY', None)
+ChatGPT_Model= os.getenv('CHATGPT_MODEL', None)
+
+chatbot = Chatbot(
+    api_key = Openai_Api_Key,
+    engine = ChatGPT_Model,
+    top_p = 1, # 0.0 ~ 1.0 # 數值越高
+    temperature = 0.5, # 0.0 ~ 2.0，數值越高越多樣化
+    presence_penalty = 0.0, # -2.0 ~ 2.0
+    frequency_penalty = 0.0, # -2.0 ~ 2.0
+    reply_count = 1
+)
+
+def ChatGPT_Reply(Ask) -> str:
     prev_text = ""
     for data in chatbot.ask(Ask):
         prev_text += data
